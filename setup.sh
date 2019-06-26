43a1b44bce1d0411df53d36ddd15493acaddcbdb
Simon C Alford <salford@mac-56-129-09.mit.edu>
2019-06-26 16:42:35 -0400

    new setup script

---
+++ setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# move this folder to .vim 
+mv ~/vim ~/.vim
+
+# make the handy trash directory
+mkdir ~/trash/
+
+if [ ! -s ~/.bashrc ]
+then
+    echo '~/.bashrc is not empty. appending contents to ~/.bash_profile'
+    echo '# automatically added by ~/.vim/setup.bash to merge with local ~/.bash_profile' >> ~/.bash_profile
+    cat ~/.bashrc >> ~/.vim/.bash_profile
+    echo '# done adding from ~/.bashrc' 
+    mv ~/.bashrc ~/trash
+    echo 'old ~/.bash_profile moved to ~/trash/'
+fi
+
+ln -s ~/.bash_profile ~/.bashrc
+echo 'linked ~/.bashrc to ~/.bash_profile'
+
+if [ ! -s ~/.bash_profile ]
+then
+    echo '~/.bash_profile is not empty. appending ~/.vim/.bash_profile to it'
+    echo '# automatically added by ~/.vim/setup.bash to merge with local ~/.bash_profile' >> ~/.vim/.bash_profile
+    cat ~/.vim/.bash_profile >> ~/.bash_profile
+    echo '# done adding from ~/.vim/.bash_profile' 
+else 
+    cp ~/.vim/.bash_profile ~/.bash_profile
+fi
+
+# assist in setting up caps lock mods
+cp ~/.vim/custom-capslock.json ~/.config/karabiner/assets/complex_modifications
+
+# assist in setting up solarized dark for terminal
+cp ~/.vim/solarized_dark.terminal ~/Documents/solarized_dark.terminal
+
